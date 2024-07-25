6fcd9041fb3e92b21ef0e73de1f3dbc18d5889ff
huguesev <Evan.Hugues@etu.univ-grenoble-alpes.fr>
2024-07-25 15:51:53 -0700

    ajout setup.py + update code

--- scraper.py
+++ scraper.py
@@ -1,9 +1,13 @@
 from telethon.sync import TelegramClient
-from telethon.tl.functions.messages import GetHistoryRequest
-from telethon.tl.types import PeerChannel
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+import os, sys
+import configparser
+import csv
+import time
 from dotenv import load_dotenv
-import os
-import asyncio 
+
+
 
 load_dotenv()
 #go to my.telegram.org
@@ -14,38 +18,45 @@ api_hash = os.getenv('API_HASH')
 phone = os.getenv('PHONE_NUMBER')
 client = TelegramClient(phone, api_id, api_hash)
 
+def banner():
+    print(f'''
+[/!\] Baudelaire scraper [/!\]  ''')
 
-# CODE ERREUR a modifier
-'''Nom du canal : Remplacez 'channel_username' par le nom du canal que vous souhaitez scraper.
-Nombre de messages : Modifiez le paramètre limit dans rate_limited_fetch pour définir le nombre de messages à récupérer.'''
-async def rate_limited_fetch(channel_username, limit=100, requests_per_second=30):
-    await client.start()
-    channel = await client.get_entity(channel_username)
-    
-    messages = []
-    offset_id = 0
-    while len(messages) < limit:
-        batch = await client(GetHistoryRequest(
-            peer=channel,
-            limit=min(100, limit - len(messages)),
-            offset_id=offset_id,
-            offset_date=None,
-            max_id=0,
-            min_id=0,
-            add_offset=0,
-            hash=0
-        ))
-        
-        if not batch.messages:
-            break
-        
-        messages.extend(batch.messages)
-        offset_id = batch.messages[-1].id
-        
-        await asyncio.sleep(1 / requests_per_second)
-    
-    for message in messages:
-        print(message.message)
+#récupération du fichier mis en place par le setup.py
+cpass = configparser.RawConfigParser()
+cpass.read('config.data')
 
-with client:
-    client.loop.run_until_complete(rate_limited_fetch('channel_username', 100))
+#bien penser à avoir le fichier .data
+try:
+    api_id = cpass['cred']['id']
+    api_hash = cpass['cred']['hash']
+    phone = cpass['cred']['phone']
+    client = TelegramClient(phone, api_id, api_hash)
+
+#si le code ne le trouve pas
+except KeyError:
+    os.system('cls') #windows only /!\, attention pour du linux mettre "clear"
+    banner()
+    print("[!] run python setup.py first !!\n")
+    sys.exit(1)
+
+#utile pour la premier connexion, apres créé un fichier .session
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    os.system('cls')
+    banner()
+    client.sign_in(phone, input('[+] Enter the code: '))
+
+os.system('cls')
+banner()
+chats = []
+last_date = None
+#chunk_size représente le nombre de messages à récupérer (max 30) sinon compte ban /!\
+chunk_size = 30
+groups=[]
+
+'''
+result = client(GetDialogsRequest(
+    ....# a finir
+    '''
---
+++ setup.py
@@ -0,0 +1,5 @@
+import os, sys
+import time
+
+
+#a cook
