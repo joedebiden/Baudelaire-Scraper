0bf8bfa0e0e4be666c1a1c96e772b31deeb5cede
huguesev <Evan.Hugues@etu.univ-grenoble-alpes.fr>
2024-07-26 14:57:52 -0700

    setup complete

--- setup.py
+++ setup.py
@@ -53,14 +53,52 @@ def merge_csv():
 	banner()
 	file1 = pd.read_csv(sys.argv[2])
 	file2 = pd.read_csv(sys.argv[3])
-	print( '[' +'+'+ ']' +' merging '+sys.argv[2]+' & '+sys.argv[3]+' ...')
-	print( '[' +'+'+ ']' +' big files can take some time ... ')
+	print(  '[+] merging '+sys.argv[2]+' & '+sys.argv[3]+' ...')
+	print(  '[+] big files can take some time ... ')
 	merge = file1.merge(file2, on='username')
 	merge.to_csv("output.csv", index=False)
-	print( '[' +'+'+ ']' +' saved file as "output.csv"\n')
+	print(  '[+] saved file as "output.csv"\n')
     
-try:
-     if len(sys.argv)<2:
-          raise IndexError
-     
-except IndexError..
+    #setup code main
+if len(sys.argv) > 1:
+    try:
+        if any([sys.argv[1] == '--config', sys.argv[1] == '-c']):
+            print('[+] selected module : ' + sys.argv[1])
+            config_setup()
+        elif any([sys.argv[1] == '--merge', sys.argv[1] == '-m']):
+            print('[+] selected module : ' + sys.argv[1])
+            merge_csv()
+        elif any([sys.argv[1] == '--install', sys.argv[1] == '-i']):
+            requirements()
+        elif any([sys.argv[1] == '--help', sys.argv[1] == '-h']):
+            banner()
+            print("""
+to merge your files do : python setup.py -m file1.csv file2.csv
+
+( --config  / -c ) setup api configuration
+( --merge   / -m ) merge 2 .csv files in one 
+( --install / -i ) install requirements
+( --help    / -h ) show this msg 
+        """)
+            
+
+        else:
+            print('\n[!] unknown argument : ' + sys.argv[1])
+            print('[!] for help use : ')
+            print(' python setup.py -h\n')
+
+
+    except IndexError:
+        print('\n[!] an argument was expected but not provided')
+        print('[!] for help use : ')
+        print(' python setup.py -h\n')
+
+
+else:
+    print('\n[!] no argument given')
+    print('[!] for help use : ')
+    print('[!] and check my github page https://github.com/joedebiden/Baudelaire-Scraper')
+    print(' python setup.py -h\n')
+
+
+# thanks to th3unkn0n
