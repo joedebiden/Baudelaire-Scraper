b3dcb0b253815265a3c9414ae9167201c97e3a02
huguesev <Evan.Hugues@etu.univ-grenoble-alpes.fr>
2024-07-27 03:18:29 -0700

    update scraper tool

--- scraper.py
+++ scraper.py
@@ -21,8 +21,27 @@ client = TelegramClient(phone, api_id, api_hash)
 '''
 
 def banner():
+    os.system('cls')
     print(f'''
-[/!\] Baudelaire scraper [/!\]  ''')
+ _                     _      _       _ 
+| |__   __ _ _   _  __| | ___| | __ _(_)_ __ ___
+| '_ \ / _` | | | |/ _` |/ _ \ |/ _` | | '__/ _ \,
+| |_) | (_| | |_| | (_| |  __/ | (_| | | | |  __/
+|_.__/ \__,_|\__,_|\__,_|\___|_|\__,_|_|_|  \___|
+
+ _       _
+| |_ ___| | ___  __ _ _ __ __ _ _ __ ___
+| __/ _ \ |/ _ \/ _` | '__/ _` | '_ ` _ \,
+| ||  __/ |  __/ (_| | | | (_| | | | | | |
+ \__\___|_|\___|\__, |_|  \__,_|_| |_| |_|
+                |___/
+
+ ___  ___ _ __ __ _ _ __   ___ _ __
+/ __|/ __| '__/ _` | '_ \ / _ \ '__|
+\__ \ (__| | | (_| | |_) |  __/ |
+|___/\___|_|  \__,_| .__/ \___|_|
+                   |_|
+''')
 
 
 cpass = configparser.RawConfigParser()
@@ -74,3 +93,42 @@ for chat in chats:
     try:
         if chat.megagroup==True:
             groups.append(chat)
+    except:
+        continue
+
+print('[+] Choose a group to scrape members:')
+i=0
+for g in groups:
+    print('[>]' + g.title)
+    i+=1
+
+print('')
+g_index=input('[+] Enter a Number: ')
+target_group=groups[int(g_index)]
+
+print('[+] Fetching Members...')
+time.sleep(1)
+all_participants = []
+all_participants = client.get_participants(target_group, aggressive=True)
+
+print('[+] Saving In file...')
+time.sleep(1)
+with open("members.csv", "w", encoding='UTF-8') as f:
+    writer = csv.writer(f, delimiter=",", alinea="\n")
+    writer.writerow(['username', 'user id', 'access hash', 'name', 'group', 'group id'])
+    for user in all_participants:
+        if user.username:
+            username= user.username
+        else:
+            username= ""
+        if user.first_name:
+            first_name= user.first_name
+        else:
+            first_name= ""
+        if user.last_name:
+            last_name= user.last_name
+        else:
+            last_name= ""
+        name= (first_name + ' ' + last_name).strip()
+        writer.writerow([username, user.id, user.access_hash, name, target_group.title, target_group.id])
+print('[+] Members scraped successfully.')
